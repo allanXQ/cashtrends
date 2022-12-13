a70a160cf05172c7ee4182010231bc61a08174ef
allanXQ <106666946+allanXQ@users.noreply.github.com>
2022-12-13 20:43:39 +0300

    added nav router

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,12 @@ import Nav from './header/nav'
 export default function Home() {
     // const router = useRouter()
     Router.push("/auth/login")
-  // return (
-  //   <div>
-  //     <Nav/>
-  //     <div>
-  //     </div>  
+  return (
+    <div>
+      <Nav/>
+      <div>
+      </div>  
 
-  //   </div>
-  // )
+    </div>
+  )
 }
