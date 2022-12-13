25d4f8899c469aa58f8c2fa2956ac92b87d73617
allanXQ <106666946+allanXQ@users.noreply.github.com>
2022-12-13 20:50:31 +0300

    added nav router

--- middleware.js
+++ middleware.js
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server'
 export async function middleware(req,res) {
     const jwt = req.cookies.get('authToken')
     const {pathname} = req.nextUrl;
-    if(pathname === '/'){
+    if(pathname == '/'){
         return NextResponse.redirect(new URL('/auth/login',req.url))
     }
     if(pathname.startsWith('/auth')){
