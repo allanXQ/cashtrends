5b6c4fc73996877d64172a1b747a65f6d3a327f3
allanXQ <106666946+allanXQ@users.noreply.github.com>
2022-12-13 20:43:28 +0300

    added nav router

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import Head from 'next/head'
-import { useRouter } from 'next/router'
-
+import Router from 'next/router'
 import Nav from './header/nav'
 
 export default function Home() {
-    const router = useRouter()
-    router.push("/auth/login")
+    // const router = useRouter()
+    Router.push("/auth/login")
   // return (
   //   <div>
   //     <Nav/>
